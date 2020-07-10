2f9ff3e6090437af5c3e03f958d70257895bd6c8
Sudipta Sen <sanborn.sen@gmail.com>
2020-07-10 14:00:28 +0530

    task create and update is added

--- src/app/containers/HomePage/slice.ts
+++ src/app/containers/HomePage/slice.ts
@@ -1,8 +1,8 @@
 import { PayloadAction } from '@reduxjs/toolkit';
 import { createSlice } from 'utils/@reduxjs/toolkit';
 import { ContainerState, Column } from './types';
-import { ColumnResponse } from 'utils/datatypes';
-import { ColMapper } from 'utils/mappers';
+import { ColumnResponse, TaskResponse } from 'utils/datatypes';
+import { ColMapper, Today, TaskMapper } from 'utils/mappers';
 
 // The initial state of the HomePage container
 export const initialState: ContainerState = {
@@ -38,6 +38,58 @@ const homePageSlice = createSlice({
       prepare: (by: number) => {
         return { payload: { by } };
       },
+    },
+    createTaskRequest: {
+      reducer: state => state,
+      prepare: (
+        markdown: string,
+        date: string,
+        column_id: string,
+        is_done: boolean,
+      ) => {
+        if (date && !column_id) {
+          const slectedDate = new Date(date).getTime();
+          const today = new Date(Today()).getTime();
+          is_done = slectedDate < today;
+        }
+        return { payload: { markdown, date, column_id, is_done } };
+      },
+    },
+    createTaskSuccess: {
+      reducer: (state, action: PayloadAction<TaskResponse>) => {
+        const task = action.payload;
+        if (task.date) {
+          const idx = state.dailyTask.findIndex(t => t.id === task.date);
+          if (idx > -1) {
+            state.dailyTask[idx].tasks.push(TaskMapper(task));
+          }
+        }
+        return state;
+      },
+      prepare: (task: TaskResponse) => ({ payload: task }),
+    },
+
+    updateTaskRequest: {
+      reducer: state => state,
+      prepare: (id: number, markdown: string, is_done: boolean) => {
+        return { payload: { markdown, is_done, id } };
+      },
+    },
+    updateTaskSuccess: {
+      reducer: (state, action: PayloadAction<TaskResponse>) => {
+        const task = action.payload;
+        if (task.date) {
+          const idx = state.dailyTask.findIndex(t => t.id === task.date);
+          if (idx > -1) {
+            const tidx = state.dailyTask[idx].tasks.findIndex(
+              t => t.id === task.id.toString(),
+            );
+            state.dailyTask[idx].tasks[tidx] = TaskMapper(task);
+          }
+        }
+        return state;
+      },
+      prepare: (task: TaskResponse) => ({ payload: task }),
     },
   },
 });
