307fac22964283a44311bccd79f1c1a823383e1a
Sudipta Sen <sanborn.sen@gmail.com>
2020-07-10 15:48:20 +0530

    drag and drop is completed

--- src/app/containers/HomePage/index.tsx
+++ src/app/containers/HomePage/index.tsx
@@ -10,13 +10,14 @@ import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components/macro';
 import { Theme, createMuiTheme } from '@material-ui/core/styles';
 import { MainSection } from 'app/components/MainSection/Loadable';
-import { DragDropContext } from 'react-beautiful-dnd';
+import { DragDropContext, OnDragEndResponder } from 'react-beautiful-dnd';
 
 import { useInjectReducer, useInjectSaga } from 'utils/redux-injectors';
 import { reducer, sliceKey, actions } from './slice';
 import { selectDailyTask, selectDailyTaskStart } from './selectors';
 import { homePageSaga } from './saga';
 import { Today, MapDateToString } from 'utils/mappers';
+import { dragNDropPayload } from './types';
 
 interface Props {
   theme?: Theme;
@@ -58,6 +59,27 @@ export const HomePage = memo((props: Props) => {
     }
   };
 
+  const handleDragNDrop = (event: OnDragEndResponder) => {
+    const payload: dragNDropPayload = {
+      taskID: event.draggableId,
+      source: {
+        colID: event.source.droppableId,
+        idx: event.source.index,
+      },
+      destination: {
+        colID: event.destination.droppableId,
+        idx: event.destination.index,
+      },
+    };
+    const colIdx = taskCols.findIndex(c => c.id === payload.destination.colID);
+    const ids = taskCols[colIdx].tasks.map(t => parseInt(t.id));
+    ids.splice(payload.destination.idx, 0, parseInt(payload.taskID));
+    dispatch(
+      actions.reposRequest({ col: payload.destination.colID, ids: ids }),
+    );
+    dispatch(actions.reposRequestLocal(payload));
+  };
+
   return (
     <>
       <Helmet>
@@ -65,7 +87,7 @@ export const HomePage = memo((props: Props) => {
         <meta name="description" content="Description of HomePage" />
       </Helmet>
       <Div>
-        <DragDropContext onDragEnd={e => console.log(e)}>
+        <DragDropContext onDragEnd={handleDragNDrop}>
           <MainSection
             taskColumns={taskCols}
             startIndex={dailyTaskStartPos}
