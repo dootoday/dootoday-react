a5a5075e6eb6b898380a573b4222ae948cbf73a6
Vikram Raj <vikrmrj@gmail.com>
2020-07-12 11:41:56 +0530

    add delete icon for done task

--- src/app/components/TaskItem/index.tsx
+++ src/app/components/TaskItem/index.tsx
@@ -13,8 +13,10 @@ import React, {
 } from 'react';
 import styled from 'styled-components/macro';
 import { createMuiTheme, Theme } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
+import { Typography, IconButton } from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
 import ReactMarkdown from 'react-markdown';
+import cx from 'classnames';
 
 export interface Task {
   id: string;
@@ -99,6 +101,11 @@ export const TaskItem = memo(
         }, 400),
       );
     };
+    const handleDelete = e => {
+      e.stopPropagation();
+      const newState = { ...taskState, ...{ markdown: '' } };
+      onTaskUpdate && onTaskUpdate(newState);
+    };
     const onBlur = (event: any) => {
       event.preventDefault();
       onTaskUpdate && onTaskUpdate({ ...taskState });
@@ -143,19 +150,30 @@ export const TaskItem = memo(
       <>
         <Div {...{ theme, highlight }}>
           {!editing && (
-            <Typography
-              onDoubleClick={handleDoublieClick}
-              onClick={handleClick}
-              variant={'caption'}
-              className={taskState.isDone ? 'done' : ''}
-            >
-              <ReactMarkdown
-                className={'md'}
-                disallowedTypes={['break', 'delete']}
-                linkTarget={'_blank'}
-                source={taskState.markdown}
-              />
-            </Typography>
+            <div className="task-item">
+              <Typography
+                onDoubleClick={handleDoublieClick}
+                onClick={handleClick}
+                variant={'caption'}
+                className={cx({ done: taskState.isDone })}
+              >
+                <ReactMarkdown
+                  className="md"
+                  disallowedTypes={['break', 'delete']}
+                  linkTarget={'_blank'}
+                  source={taskState.markdown}
+                />
+                {taskState.isDone && (
+                  <IconButton
+                    aria-label="delete"
+                    className={cx('icon-button', 'icon-button-delete')}
+                    onClick={handleDelete}
+                  >
+                    <DeleteIcon fontSize="inherit" />
+                  </IconButton>
+                )}
+              </Typography>
+            </div>
           )}
           {(editing || isJustInput) && (
             <input
@@ -186,7 +204,6 @@ const Div = styled.div<{ theme: Theme; highlight: boolean }>`
     props.highlight
       ? props.theme.palette.primary.dark
       : props.theme.palette.secondary.dark};
-
   .done {
     text-decoration: line-through;
     color: ${props =>
@@ -217,6 +234,31 @@ const Div = styled.div<{ theme: Theme; highlight: boolean }>`
         ? props.theme.palette.primary.dark
         : props.theme.palette.secondary.dark};
   }
+  .icon-button {
+    padding: 0;
+    font-size: 16px;
+    position: absolute;
+    right: 0;
+    top: 1px;
+  }
+  .task-item {
+    position: relative;
+    .icon-button-delete {
+      display: none;
+    }
+    :hover {
+      background-color: ${props => props.theme.palette.primary.light};
+      .icon-button-delete {
+        display: block;
+        :hover {
+          color: ${props =>
+            props.highlight
+              ? props.theme.palette.primary.dark
+              : props.theme.palette.secondary.dark};
+        }
+      }
+    }
+  }
   .md {
     width: 100%;
     white-space: nowrap;
@@ -227,7 +269,6 @@ const Div = styled.div<{ theme: Theme; highlight: boolean }>`
       overflow: visible;
       text-overflow: unset;
       white-space: initial;
-      background-color: ${props => props.theme.palette.primary.light};
       cursor: grab;
       position: relative;
     }
@@ -281,6 +322,17 @@ const Div = styled.div<{ theme: Theme; highlight: boolean }>`
       }
       li {
         font-size: 0.95rem;
+      }
+    }
+  }
+  @media only screen and (max-width: 768px) {
+    .icon-button {
+      right: 1px;
+      top: 5px;
+    }
+    .task-item {
+      .icon-button-delete {
+        display: block;
       }
     }
   }
