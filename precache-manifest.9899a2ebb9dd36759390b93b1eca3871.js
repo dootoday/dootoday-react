7f1e2b88509465984cccb9a905eafabf5dda3088
Sudipta Sen <sanborn.sen@gmail.com>
2020-07-12 19:19:17 +0530

    Updates

---
+++ precache-manifest.9899a2ebb9dd36759390b93b1eca3871.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac7b003a57e451168eaca6ae3415232a",
+    "url": "/dootoday-react/index.html"
+  },
+  {
+    "revision": "fe3e4fde7fc165a9d040",
+    "url": "/dootoday-react/static/css/3.d54bb455.chunk.css"
+  },
+  {
+    "revision": "1548b82bf85439f0ac83",
+    "url": "/dootoday-react/static/js/0.936a4dc5.chunk.js"
+  },
+  {
+    "revision": "fe3e4fde7fc165a9d040",
+    "url": "/dootoday-react/static/js/3.268ee6f2.chunk.js"
+  },
+  {
+    "revision": "5fe809f7c5187d2232bb1a98d278ed3e",
+    "url": "/dootoday-react/static/js/3.268ee6f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef924f6353d0a3a5a60e",
+    "url": "/dootoday-react/static/js/4.24082c6a.chunk.js"
+  },
+  {
+    "revision": "e4a0d87becbcc6d09668875fe197b50f",
+    "url": "/dootoday-react/static/js/4.24082c6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef01811ff69e04db1345",
+    "url": "/dootoday-react/static/js/5.3df79af4.chunk.js"
+  },
+  {
+    "revision": "954990b9b448da967741",
+    "url": "/dootoday-react/static/js/6.86d1ba3b.chunk.js"
+  },
+  {
+    "revision": "8c914e1092864f8237cd",
+    "url": "/dootoday-react/static/js/7.d91a711f.chunk.js"
+  },
+  {
+    "revision": "021119ccbdb2c1c65f9d",
+    "url": "/dootoday-react/static/js/8.e61cb62d.chunk.js"
+  },
+  {
+    "revision": "a55485c4513ffdb10e15",
+    "url": "/dootoday-react/static/js/9.5dd3a19f.chunk.js"
+  },
+  {
+    "revision": "6eeb98b563ebfb6159b7",
+    "url": "/dootoday-react/static/js/main.a30b72d3.chunk.js"
+  },
+  {
+    "revision": "4ba844d707c2e786eb04",
+    "url": "/dootoday-react/static/js/runtime-main.4e40ceaa.js"
+  }
+]);
