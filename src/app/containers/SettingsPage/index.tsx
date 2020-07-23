a8dba75780c2c8c81079a323348465e423b460bf
Sudipta Sen <sanborn.sen@gmail.com>
2020-07-23 08:01:51 +0530

    basic navigation setup done

--- src/app/containers/SettingsPage/index.tsx
+++ src/app/containers/SettingsPage/index.tsx
@@ -7,8 +7,10 @@
 import React, { memo } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useSelector, useDispatch } from 'react-redux';
+import { Route, Switch, NavLink } from 'react-router-dom';
 import styled from 'styled-components/macro';
 
+import { SubscribePage } from 'app/containers/SubscribePage';
 import { useInjectReducer, useInjectSaga } from 'utils/redux-injectors';
 import { reducer, sliceKey } from './slice';
 import { selectSettingsPage } from './selectors';
@@ -18,11 +20,15 @@ import {
   Grid,
   List,
   ListItem,
-  Divider,
   ListItemText,
+  Theme,
+  createMuiTheme,
+  Typography,
 } from '@material-ui/core';
 
-interface Props {}
+interface Props {
+  theme?: Theme;
+}
 
 export const SettingsPage = memo((props: Props) => {
   useInjectReducer({ key: sliceKey, reducer: reducer });
@@ -32,6 +38,7 @@ export const SettingsPage = memo((props: Props) => {
   const settingsPage = useSelector(selectSettingsPage);
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const dispatch = useDispatch();
+  const theme = props.theme || createMuiTheme();
 
   return (
     <>
@@ -40,28 +47,44 @@ export const SettingsPage = memo((props: Props) => {
         <meta name="description" content="Description of SettingsPage" />
       </Helmet>
       <Container maxWidth="md">
-        <Div>
+        <Div theme={theme}>
           <Grid container spacing={1} direction="row">
             <Grid item container md={4} lg={4} sm={12}>
               <List component="nav" className="nav-list" aria-label="settings">
-                <ListItem button>
-                  <ListItemText primary="Inbox" />
-                </ListItem>
-                <Divider />
-                <ListItem button divider>
-                  <ListItemText primary="Drafts" />
-                </ListItem>
-                <ListItem button>
-                  <ListItemText primary="Trash" />
-                </ListItem>
-                <Divider light />
-                <ListItem button>
-                  <ListItemText primary="Spam" />
-                </ListItem>
+                <NavLink
+                  exact
+                  to="/me"
+                  className="nav-link"
+                  activeClassName="active"
+                >
+                  <ListItem button className="nav-item">
+                    <Typography>Profile</Typography>
+                  </ListItem>
+                </NavLink>
+                <NavLink
+                  to="/me/subscription"
+                  className="nav-link"
+                  activeClassName="active"
+                >
+                  <ListItem button className="nav-item">
+                    <ListItemText primary="Subscription" />
+                  </ListItem>
+                </NavLink>
               </List>
             </Grid>
             <Grid item container md={8} lg={8} sm={12}>
-              <div>This is the content</div>
+              <Switch>
+                <Route
+                  exact
+                  path="/me/subscription"
+                  component={() => <SubscribePage theme={theme} />}
+                />
+                <Route
+                  exact
+                  path="/me/test"
+                  component={() => <div>Hello</div>}
+                />
+              </Switch>
             </Grid>
           </Grid>
         </Div>
@@ -70,11 +93,20 @@ export const SettingsPage = memo((props: Props) => {
   );
 });
 
-const Div = styled.div`
+const Div = styled.div<{ theme: Theme }>`
   margin-top: 30px;
   min-height: calc(100vh - 127px);
 
   .nav-list {
     width: 100%;
+    .nav-link {
+      text-decoration: none;
+      color: ${props => props.theme.palette.primary.dark};
+      &.active {
+        .nav-item {
+          background-color: ${props => props.theme.palette.primary.light};
+        }
+      }
+    }
   }
 `;
