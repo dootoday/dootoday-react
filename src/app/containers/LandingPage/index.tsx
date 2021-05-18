844f18324f481455ad852f55176c6e13fa24d4d9
Sudipta Sen <sanborn.sen@gmail.com>
2021-05-18 17:21:15 +0530

    closes #51 : intial home page integrated

--- src/app/containers/LandingPage/index.tsx
+++ src/app/containers/LandingPage/index.tsx
@@ -6,9 +6,13 @@
 
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
-import styled from 'styled-components/macro';
-import { PublicLayout } from 'app/components/PublicLayout';
-import { Container, Typography, Button, Grid } from '@material-ui/core';
+import { Div } from './style';
+import logo from 'assets/logo1.png';
+import pic1 from 'assets/pic1.png';
+import pic2 from 'assets/pic2.png';
+import pic3 from 'assets/pic3.png';
+import pic4 from 'assets/pic4.png';
+import todo from 'assets/todo.png';
 import { Link } from 'react-router-dom';
 
 interface Props {}
@@ -21,67 +25,167 @@ export function LandingPage(props: Props) {
         <meta name="description" content="Description of LandingPage" />
       </Helmet>
       <Div>
-        <PublicLayout>
-          <Container>
-            <Grid container justify="center" direction="column" spacing={5}>
-              <Grid item>
-                <Typography
-                  variant="h5"
-                  align="center"
-                  className="heared1"
-                  color="primary"
-                >
-                  Simplest Daily Task Manager ever
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography variant="h6" align="center">
-                  We know you have a lot to do today! <br />
-                  So, let's not waste time on any fancy Landing Page.
-                </Typography>
-              </Grid>
-              <Grid item className="signup-button">
-                <Link to="/login">
-                  <Button variant="contained" size="large" color="primary">
-                    Let's Dive Right Into It
-                  </Button>
+        <header className="header">
+          <nav className="nav">
+            <input id="nav-toggle" type="checkbox" />
+            <div className="logo">
+              <img src={logo} alt="" className="logo-img" />
+            </div>
+            <ul className="desktop-links">
+              <li className="list-item">
+                <a href="#home">Home</a>
+              </li>
+              <li className="list-item">
+                <a href="#pricing">Pricing</a>
+              </li>
+              <li className="list-item">
+                <a href="#pricing">Join</a>
+              </li>
+              <li className="list-item">
+                <a href="#ping_us">Ping Us</a>
+              </li>
+            </ul>
+            <ul id="mobile-links" className="mobile-links">
+              <li className="list-item">
+                <a href="#home">Home</a>
+              </li>
+              <li className="list-item">
+                <a href="#pricing">Pricing</a>
+              </li>
+              <li className="list-item">
+                <a href="#pricing">Join</a>
+              </li>
+              <li className="list-item">
+                <a href="#ping_us">Ping Us</a>
+              </li>
+            </ul>
+            <label className="icon-burger">
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+            </label>
+          </nav>
+        </header>
+        <main className="container">
+          <section className="section" id="home">
+            <div>
+              <h1 className="heading">What do</h1>
+              <h3 className="meta" id="youwant">
+                You want to accomplish today?
+              </h3>
+              <p className="sub-meta">
+                Get organised &nbsp; &nbsp; Get determined &nbsp; &nbsp; Get
+                going
+              </p>
+            </div>
+            <div>
+              <img src={pic1} alt="" className="responsive-image" />
+            </div>
+          </section>
+
+          <section className="section">
+            <div className="third">
+              <img src={pic2} alt="" className="responsive-image" />
+            </div>
+
+            <div className="forth">
+              <h1 className="heading alignr">Simple</h1>
+              <h3 className="meta alignr">as pen on papper</h3>
+              <p className="sub-meta alignr">Got something on your mind?</p>
+              <p className="sub-meta alignr">
+                Start writing it down. No new section, no new <br />
+                page. Pen it and move on.
+              </p>
+            </div>
+          </section>
+
+          <section className="section">
+            <div>
+              <h3 className="meta">We got colors to match your mood</h3>
+              <h3 className="meta">Take your pick!</h3>
+              <p className="sub-meta">
+                Or, Let us your diary for a refresh look
+              </p>
+            </div>
+
+            <div>
+              <img className="responsive-image" src={pic1} alt="" />
+            </div>
+          </section>
+
+          <section className="section" id="pricing">
+            <div className="subscribe_card"></div>
+
+            <div className="joinus">
+              <h3 className="meta alignr">Less then</h3>
+              <div className="alignr sameline">
+                <h3 className="meta suffix-spcl-case">a</h3>
+                <h1 className="heading">buck</h1>
+              </div>
+              <h3 className="meta">when you subscribe</h3>
+              <Link to="/login" className="alignr join">
+                JOIN NOW
+              </Link>
+            </div>
+          </section>
+
+          <section className="section">
+            <div>
+              <h3 className="meta">Put a</h3>
+              <h1 className="heading">Stop</h1>
+              <h3 className="meta">to forgetting your deadline.</h3>
+              <h3 className="meta">Come join us today</h3>
+            </div>
+
+            <div>
+              <img src={pic3} alt="" className="responsive-image" />
+            </div>
+          </section>
+
+          <footer id="ping_us">
+            <section className="section">
+              <div>
+                <img src={todo} alt="" className="responsive-image" />
+              </div>
+              <div>
+                <h1 className="meta">Buzz about us</h1>
+                <p className="para">
+                  Heart be still i loved this place. Way better than i expeted.
+                  I had the spicy noodles and they were delicious,
+                </p>
+                <p className="para">flavor great and quality was on point.</p>
+                <p className="para">
+                  For desert the sticky rice with mango, i dream about it now.
+                </p>
+                <p className="para">Highly recomend if you are in the mood</p>
+                <h2 className="alignr"> - Rishi Mukherjee</h2>
+              </div>
+            </section>
+            <section>
+              <div>
+                <img src={pic4} alt="" className="last-img" />
+              </div>
+              <nav className="foot-nav">
+                <Link className="foot-nav-item" to="/aboutus">
+                  About us
                 </Link>
-              </Grid>
-              <Grid item>
-                <Typography variant="h6" align="center">
-                  {`If you still love a fancy landing page, 
-                  let us know. We'll add it to our task list.`}
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography variant="body1" align="center">
-                  Not Important! But in case if you want to know more, <br />
-                  here is little something{' '}
-                  <Link className="about-us" to="/aboutus">
-                    About Us.
-                  </Link>
-                </Typography>
-              </Grid>
-            </Grid>
-          </Container>
-        </PublicLayout>
+                <Link className="foot-nav-item" to="/contact">
+                  Contact Us
+                </Link>
+                <Link className="foot-nav-item" to="/privacy">
+                  Privacy Policy
+                </Link>
+                <Link className="foot-nav-item" to="/tnc">
+                  Terms and Condition
+                </Link>
+                <Link className="foot-nav-item" to="/refund-policy">
+                  Refund Policy
+                </Link>
+              </nav>
+            </section>
+          </footer>
+        </main>
       </Div>
     </>
   );
 }
-
-const Div = styled.div`
-  .heared1 {
-    text-transform: uppercase;
-  }
-  .signup-button {
-    margin: auto;
-    a {
-      text-decoration: none;
-    }
-  }
-  .about-us {
-    text-decoration: none;
-    font-style: italic;
-  }
-`;
---
+++ src/app/containers/LandingPage/style.tsx
@@ -0,0 +1,293 @@
+import styled from 'styled-components/macro';
+
+export const Div = styled.div`
+  font-family: 'Roboto', sans-serif;
+  display: inline-block;
+  width: 100%;
+  margin: 0 !important;
+  font-size: 20px;
+  line-height: normal;
+
+  .header {
+    width: 100%;
+    height: 10vh;
+    position: fixed;
+    background-color: white;
+    z-index: 10;
+    left: 0;
+    top: 0;
+  }
+
+  .nav {
+    height: 100%;
+    width: 95vw;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: auto;
+  }
+  .nav .desktop-links {
+    display: flex;
+    padding-inline-start: 0;
+  }
+  .nav .desktop-links .list-item {
+    border-bottom: 2px solid #ffffff;
+    transition: all 0.4s linear;
+    list-style: none;
+    margin: 0px 20px;
+  }
+  .nav .desktop-links .list-item:hover {
+    border-color: #0d6c8c;
+  }
+  .nav .desktop-links .list-item a {
+    color: #02335a;
+    text-decoration: none;
+    text-transform: uppercase;
+  }
+
+  .nav .mobile-links {
+    position: absolute;
+    height: 90vh;
+    z-index: 12;
+    background: #ffffff;
+    top: 0;
+    margin-top: 10vh;
+    width: 100%;
+    display: none;
+    margin-left: 0px;
+    margin-right: 0px;
+    padding: 0px;
+  }
+
+  .nav .mobile-links .list-item {
+    list-style: none;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .nav .mobile-links .list-item a {
+    text-decoration: none;
+    text-transform: uppercase;
+    color: #02335a;
+    margin: auto;
+    font-size: 1.8em;
+  }
+
+  .logo {
+    height: 50%;
+  }
+  .logo-img {
+    height: 100%;
+  }
+
+  #nav-toggle {
+    position: absolute;
+    top: -100px;
+  }
+  .nav .icon-burger {
+    display: none;
+    position: absolute;
+    right: 15px;
+  }
+  .nav .icon-burger .line {
+    width: 20px;
+    height: 3px;
+    background-color: #0d6c8c;
+    margin: 3px;
+    border-radius: 3px;
+    transition: all 0.3s ease-in-out;
+  }
+
+  .container {
+    margin-top: 10vh;
+    display: block;
+    height: 90vh;
+    overflow-y: auto;
+    overflow-x: hidden;
+    scrollbar-width: none;
+  }
+
+  .heading {
+    font-weight: 200;
+    font-size: 4em;
+    margin: 5px 0px;
+  }
+
+  .meta {
+    font-weight: 100;
+    font-size: 2em;
+    margin: 5px 0px;
+  }
+
+  .sub-meta {
+    font-size: 0.9em;
+    font-weight: 100;
+  }
+
+  .alignr {
+    text-align: right;
+  }
+
+  .sameline {
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  .suffix-spcl-case {
+    align-self: flex-end;
+    margin-bottom: 15px;
+    margin-right: 5px;
+  }
+
+  /* Image*/
+  .responsive-image {
+    width: 90%;
+  }
+
+  .section {
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    width: 95vw;
+    margin: auto;
+  }
+
+  .forth {
+    min-width: 280px;
+  }
+
+  /* ---------------------------------1st section-------------------------------- */
+
+  .subscribe_card {
+    width: 22rem;
+    height: 31rem;
+    background: #0d6c8c;
+    border-radius: 2rem;
+    margin-top: 7rem;
+  }
+
+  /* join button */
+  .join {
+    margin-top: 2.25px;
+    text-decoration: none;
+    font-size: 2rem;
+    padding: 1px 30px;
+    border: 1px solid #0d6c8c;
+    color: black;
+    position: relative;
+    z-index: 1;
+    float: right;
+  }
+  .join::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: #0d6c8c;
+    width: 100%;
+    height: 100%;
+    transform: translateX(100%);
+    transition: all 0.18s linear;
+  }
+  .join:hover:before {
+    transform: translateX(0);
+    z-index: -1;
+  }
+  .join:hover {
+    color: white;
+  }
+  .joinus {
+    overflow: hidden;
+  }
+
+  /* -------------------------------Footer------------------------------------- */
+
+  .para {
+    margin-top: 1rem;
+  }
+
+  /* ----------------------------2nd Nav Section------------------------------- */
+
+  .last-img {
+    width: 100%;
+    text-decoration: none;
+  }
+  .foot-nav {
+    display: flex;
+    justify-content: space-evenly;
+    margin-bottom: 20px;
+  }
+
+  .foot-nav .foot-nav-item {
+    text-decoration: none;
+    font-size: 0.7em;
+    color: black;
+  }
+
+  @media (max-width: 735px) {
+    text-align: center;
+    .nav {
+      justify-content: center;
+    }
+
+    .nav .desktop-links {
+      display: none;
+    }
+
+    .nav .mobile-links {
+      display: none;
+      margin-top: 7vh;
+      height: 93vh;
+    }
+
+    .header {
+      height: 7vh;
+    }
+
+    .container {
+      margin-top: 7vh;
+      height: 93vh;
+    }
+    .section {
+      flex-direction: column;
+    }
+    .alignr {
+      text-align: center;
+    }
+    .third {
+      order: 4;
+    }
+    .forth {
+      order: 3;
+    }
+    .subscribe_card {
+      order: 8;
+      width: 80%;
+      height: 400px;
+      margin-top: 20px;
+      margin-bottom: 40px;
+    }
+    .joinus {
+      order: 7;
+    }
+    .responsive-image {
+      width: 100%;
+      height: auto;
+    }
+
+    .nav .icon-burger {
+      display: block;
+    }
+  }
+
+  /* Font sizes */
+  @media (max-width: 900px) {
+    font-size: 14px;
+  }
+
+  @media (max-width: 390px) {
+    font-size: 11px;
+  }
+`;
